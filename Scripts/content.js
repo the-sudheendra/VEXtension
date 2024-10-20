636d8ccd37f10ded3dab815787d8ae27149e7806
the-sudheendra <sudheendramurugla@gmail.com>
2024-10-20 19:24:33 +0530

    Saving the changes

--- Scripts/content.js
+++ Scripts/content.js
@@ -1,16 +1,29 @@
+// Declaration
 const veXButtonId = "veX-Button";
 const veXPopUpId = "veX-PopUp-Container"
+const veXPopUpOverlayId = "veX-PopUp-Overlay";
 const veXButtonNode = document.createElement("div");
 const veXPopUpNode = document.createElement("div");
-document.body.appendChild(veXButtonNode);
-document.body.appendChild(veXPopUpNode);
+const veXPopUpOverlay = document.createElement("div");
+const titleNode = document.head.querySelector('title');
 
-let isveXPopOpen = false;
-veXPopUpNode.style.display = "none";
+var veXMutationObservers = {};
+var veXMutationObserversConfig = {};
+var veXGlobalId = false;
 veXPopUpNode.id = veXPopUpId;
 veXButtonNode.id = veXButtonId;
-veXButtonNode.innerHTML="🔼";
+//Logic
+document.body.appendChild(veXButtonNode);
+document.body.appendChild(veXPopUpNode);
+document.body.appendChild(veXPopUpOverlay);
 
+var isveXPopOpen = false;
+veXPopUpNode.style.display = "none";
+// veXButtonNode.style.display = "none";
+
+veXPopUpOverlay.id = veXPopUpOverlayId;
+veXButtonNode.innerHTML = "✔";
+veXPopUpOverlay.addEventListener("click", closeveXPopUp);
 const dods = {
   dod: {
     title: "What is the Definition of Done?",
@@ -104,55 +117,123 @@ const dods = {
     ]
   }
 };
+this.veXMutationObserversConfig =
+{
+  titleObserver:
+  {
+    id: "title",
+    targetNode: document.head.querySelector('title'),
+    options: { attributes: true, childList: true, subtree: true },
+    callback: (mutationList, observer) => {
+      for (const mutation of mutationList) {
+        onTitleChange();
+      }
+    }
+  }
+  
+//   body:
+//   {
+//     id: "body",
+//     targetNode: document.querySelector("body"),
+//     options: { attributes: true, childList: true, subtree: true },
+//     callback: (mutationList, observer) => {
+//       for (const mutation of mutationList) {
+//         // this.veXGlobalId = document.querySelector("[title='Global ID - This ID is unique across all Octane workspaces managed by the Software Factory']") ? true : false;
+//         // if (this.veXGlobalId) {
+//         //   veXButtonNode.style.display = "flex";
+//         //   if(document.querySelector(".entity-form-fields-container") && this.veXPopUpNode)
+//         //     {
+//         //       document.querySelector(".entity-form-fields-container").appendChild(this.veXPopUpNode);
+//         //     }
+//         // }
+//         // else {
+//         //   veXButtonNode.style.display = "none";
+//         // }
 
+//       }
+//     }
+//   }
 
-veXPopUpNode.innerHTML =
-  `
-    <header>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=checklist" /> 
-   <h1>Defination Of Done</h1>
+}
+var veXPopUI = `
+<header class="veX_header" >
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=checklist" /> 
+<h1>Defination Of Done</h1>
 </header>
 <div class="veX_content-wrapper">
-   <div class="veX_sidebar">
-      <button id="veX_sidebar_epic" name="epic">Epic</button>
-      <button id="veX_sidebar_feature" name="feature">Feature</button>
-      <button id="veX_sidebar_enhancement" name="enhancement" >Enhancement</button>
-      <button id="veX_sidebar_cpeIncident" name="cpeincident">CPE Incident</button>
-      <button id="veX_sidebar_userStory" name="userstory">UserStory</button>
-      <button id="veX_sidebar_spike" name="spike" >Spike</button>
-      <button id="veX_sidebar_documentation" name="documentation">Documentation</button>
-   </div>
-   <div class="veX_main-content">
-      <div id="tiles" class="veX_tab-content">
-         <div class="veX_dod_title">a</div>
-         <div class="veX_dod_desc">a</div>
-         <div class="veX_dod_list_container">
-            <ul class="veX_dod_list" id="myUL">
-               <li>Documentation is complete and up-to-date.</li>
-               <li>Reviewed by relevant team members.</li>
-               <li>Stored in the correct repository.</li>
-               <li>Stakeholders have reviewed and approved.</li>
-            </ul>
-         </div>
-      </div>
-   </div>
+<div class="veX_sidebar">
+  <button id="veX_sidebar_epic" name="epic">Epic</button>
+  <button id="veX_sidebar_feature" name="feature">Feature</button>
+  <button id="veX_sidebar_enhancement" name="enhancement" >Enhancement</button>
+  <button id="veX_sidebar_cpeIncident" name="cpeincident">CPE Incident</button>
+  <button id="veX_sidebar_userStory" name="userstory">UserStory</button>
+  <button id="veX_sidebar_spike" name="spike" >Spike</button>
+  <button id="veX_sidebar_documentation" name="documentation">Documentation</button>
 </div>
-`
+<div class="veX_main-content">
+  <div id="tiles" class="veX_tab-content">
+     <div class="veX_dod_title">a</div>
+     <div class="veX_dod_desc">a</div>
+     <div class="veX_dod_list_container">
+        <ul class="veX_dod_list" id="myUL">
+           <li>Documentation is complete and up-to-date.</li>
+           <li>Reviewed by relevant team members.</li>
+           <li>Stored in the correct repository.</li>
+           <li>Stakeholders have reviewed and approved.</li>
+        </ul>
+     </div>
+  </div>
+</div>
+</div>
+<div class="veX_footer">
+<button class="veX_add_to-comment-btn">Leave a Comment</button>
+<button class="veX_copy_done_items_btn">Copy Done Items</button>
+</div>
+</div>
+`;
 
+veXPopUpNode.innerHTML = this.veXPopUI;
+
+this.initMutationObservers();
+function initMutationObservers() {
+  Object.keys(this.veXMutationObserversConfig).forEach(
+    key => {
+      let mutationParams = this.veXMutationObserversConfig[key];
+      if (mutationParams) {
+        let mutationObserver = new MutationObserver(mutationParams.callback);
+        if(mutationParams.targetNode && mutationParams.options)
+        mutationObserver.observe(mutationParams.targetNode, mutationParams.options);
+        this.veXMutationObservers[mutationParams.id] = mutationObserver;
+      }
+    }
+  );
+
+}
+
+
+function addToComments() {
+  document.querySelectorAll(".mqm-new-comment-div")[0].childNodes[2].click();
+  document.querySelectorAll(".mqm-new-comment-div")[0].childNodes[2].click();
+  document.querySelectorAll(".fr-wrapper")[2].childNodes[0].innerHTML = '<h1>hi<h1>'
+  document.querySelectorAll('ng-click="comments.onAddNewCommentClicked()"');
+
+}
 veXButtonNode.addEventListener('click', toggleveXPopup)
 function toggleveXPopup() {
-  this.isveXPopOpen = !this.isveXPopOpen;
-  if (this.isveXPopOpen) {
-    veXPopUpNode.style.display = 'block';
-    veXButtonNode.innerHTML="🔽"
-
+  if (veXButtonNode.innerHTML == "🔽") {
+    closeveXPopUp();
   }
   else {
-    veXPopUpNode.style.display = 'none';
-    veXButtonNode.innerHTML="🔼"
+    openVexPopUp();
   }
 }
 
+function openVexPopUp() {
+
+  veXPopUpOverlay.style.visibility = "visible";
+  veXPopUpNode.style.display = 'block';
+  veXButtonNode.innerHTML = "🔽"
+}
 function showTab(tabId) {
 
   let tabTitle = document.getElementsByClassName('veX_dod_title')[0];
@@ -176,22 +257,20 @@ function addClickEventForSideBarTab() {
   });
 
 }
-function updateTheListView(listItems)
-{
- let list = document.getElementsByClassName("veX_dod_list")[0];
- list.innerHTML='';
- listItems.forEach(item => {
-  const li = document.createElement('li');
-  li.textContent = item;
-  list.appendChild(li);
-})
+function updateTheListView(listItems) {
+  let list = document.getElementsByClassName("veX_dod_list")[0];
+  list.innerHTML = '';
+  listItems.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = item;
+    list.appendChild(li);
+  })
 
 
 }
 addClickEventForSideBarTab();
 showTab('dod');
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
+var myNodelist = document.getElementsByTagName("li");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
@@ -201,25 +280,22 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
-// Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
+list.addEventListener('click', function (ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
 }, false);
 
-// Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
@@ -239,9 +315,97 @@ function newElement() {
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
     }
   }
 }
+function isThisVETicket() {
+  document.querySelector("[title='Global ID - This ID is unique across all Octane workspaces managed by the Software Factory']") ? true : false;
+}
+
+// const closeBtn = document.querySelector('.veX_add_to-comment-btn');
+// veXPopUpOverlay.addEventListener('click',)
+// // Function to show the pop-up
+// function showPopup() {
+//   popupOverlay.classList.add('show');
+// }
+
+// // Function to hide the pop-up
+// function hidePopup() {
+//   popupOverlay.classList.remove('show');
+// }
+
+
+// closeBtn.addEventListener('click', hidePopup);
+
+// Events
+
+function closeveXPopUp() {
+  veXPopUpOverlay.style.visibility = "hidden";
+  veXPopUpNode.style.display = 'none';
+  veXButtonNode.innerHTML = "✔"
+}
+
+function addDoneListToComments() {
+  try {
+    document.querySelectorAll(".mqm-new-comment-div")[0].childNodes[2].click();
+    document.querySelectorAll(".mqm-new-comment-div")[0].childNodes[2].click();
+    document.querySelectorAll(".fr-wrapper")[2].childNodes[0].innerHTML = '<h1>hi<h1>'
+    document.querySelectorAll('ng-click="comments.onAddNewCommentClicked()"');
+  }
+  catch (ex) {
+    console.error("Exception occured: " + ex);
+  }
+
+}
+
+
+function setHeaderTextForPopUp(title) {
+
+  if (document.querySelector(".veX_header")) {
+    document.querySelector(".veX_header").innerText = title;
+  }
+
+}
+//Utilities
+
+function getVETicketInfo(title) {
+  ticketArr = title.split(" ");
+  if (ticketArr.length >= 2) {
+    const match = ticketArr[0].match(/^([a-zA-Z]+)(\d+)$/);
+    if (match) {
+      return {
+        ticketType: getExpandedTicketType(match[1]),
+        ticketID: match[2],
+        ticketTitle:title.slice(ticketArr[0].length+1)
+      };
+    } else {
+      return null;
+    }
+  }
+  else
+    return null;
+}
+
+function getExpandedTicketType(ticketType) {
+  switch (ticketType) {
+    case 'US':
+      return 'userstory';
+    default: return 'ticket';
+  }
+}
+
+function onTitleChange() {
+  let newTitle = document.head.querySelector('title').innerText;
+  let ticketInfo = getVETicketInfo(newTitle);
+  if(ticketInfo)
+  this.setHeaderTextForPopUp(`[${ticketInfo.ticketID}]-${conciseText(ticketInfo.ticketTitle)}`);
+}
+function conciseText(text)
+{
+  if(text.length<=40) return text;
+  return text.slice(0,20)+"..."+text.slice(-20);
+}
+
